2677e1fe028e91bc880650a94bd76d12d1ea2d11
Priyansh-Mathur <priyanshmathur1105@gmail.com>
2026-05-20 01:10:22 +0530

    embedded MANIT Google custom map

--- frontend/src/components/ManitMap.jsx
+++ frontend/src/components/ManitMap.jsx
@@ -1,56 +1,15 @@
-import { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Polygon } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import * as toGeoJSON from "@tmcw/togeojson";
-
 export default function ManitMap() {
-  const [polygons, setPolygons] = useState([]);
-
-  useEffect(() => {
-    fetch("/Untitled layer.kml")
-      .then((res) => res.text())
-      .then((kmlText) => {
-        const parser = new DOMParser();
-        const kml = parser.parseFromString(kmlText, "text/xml");
-
-        const geojson = toGeoJSON.kml(kml);
-        const extracted = [];
-
-        geojson.features.forEach((feature) => {
-          if (feature.geometry && feature.geometry.type === "Polygon") {
-            extracted.push(
-              feature.geometry.coordinates[0].map(([lng, lat]) => [lat, lng])
-            );
-          }
-        });
-
-        setPolygons(extracted);
-      });
-  }, []);
-
   return (
-    <MapContainer
-      center={[23.213, 77.406]}
-      zoom={15}
-      style={{ height: "500px", width: "100%" }}
-    >
-      <TileLayer
-        attribution="&copy; OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <div style={{ width: "100%", height: "500px" }}>
+      <iframe
+        title="MANIT Campus Map"
+        src="https://www.google.com/maps/d/embed?mid=1D1OCIlq49qF4mNJKGKJ5YeAk_Deulpw"
+        width="100%"
+        height="500"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
       />
-
-      {polygons.map((polygon, index) => (
-        <Polygon
-          key={index}
-          positions={polygon}
-          pathOptions={{
-            color: "green",
-            fillColor: "green",
-            fillOpacity: 0.3,
-            weight: 3,
-          }}
-        />
-      ))}
-    </MapContainer>
+    </div>
   );
 }
