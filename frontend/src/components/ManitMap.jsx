f4fb7908ea456d6174de803d28f413959b8b0fdd
Priyansh-Mathur <priyanshmathur1105@gmail.com>
2026-05-20 00:54:38 +0530

    added exact MANIT KML boundary map

--- frontend/src/components/ManitMap.jsx
+++ frontend/src/components/ManitMap.jsx
@@ -1,23 +1,33 @@
+import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Polygon } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
-const boundary = [
-  [23.2211481, 77.403829],
-  [23.2177169, 77.3973488],
-  [23.215666, 77.3953747],
-  [23.2139306, 77.3943018],
-  [23.2122346, 77.3961043],
-  [23.2091187, 77.3973917],
-  [23.2063577, 77.4012541],
-  [23.2045433, 77.4070906],
-  [23.2065944, 77.4133563],
-  [23.2112486, 77.4173045],
-  [23.2157449, 77.4170899],
-  [23.2207142, 77.4103951],
-  [23.2211481, 77.403829],
-];
+import * as toGeoJSON from "@tmcw/togeojson";
 
 export default function ManitMap() {
+  const [polygons, setPolygons] = useState([]);
+
+  useEffect(() => {
+    fetch("/Untitled map.kml")
+      .then((res) => res.text())
+      .then((kmlText) => {
+        const parser = new DOMParser();
+        const kml = parser.parseFromString(kmlText, "text/xml");
+
+        const geojson = toGeoJSON.kml(kml);
+        const extracted = [];
+
+        geojson.features.forEach((feature) => {
+          if (feature.geometry && feature.geometry.type === "Polygon") {
+            extracted.push(
+              feature.geometry.coordinates[0].map(([lng, lat]) => [lat, lng])
+            );
+          }
+        });
+
+        setPolygons(extracted);
+      });
+  }, []);
+
   return (
     <MapContainer
       center={[23.213, 77.406]}
@@ -25,19 +35,22 @@ export default function ManitMap() {
       style={{ height: "500px", width: "100%" }}
     >
       <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
+        attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <Polygon
-        positions={boundary}
-        pathOptions={{
-          color: "green",
-          fillColor: "green",
-          fillOpacity: 0.3,
-          weight: 3,
-        }}
-      />
+      {polygons.map((polygon, index) => (
+        <Polygon
+          key={index}
+          positions={polygon}
+          pathOptions={{
+            color: "green",
+            fillColor: "green",
+            fillOpacity: 0.3,
+            weight: 3,
+          }}
+        />
+      ))}
     </MapContainer>
   );
 }
